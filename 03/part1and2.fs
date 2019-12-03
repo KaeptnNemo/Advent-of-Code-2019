06771e7b3cf9a91ca2a2994cd00eedc6d094e198
Alexander Schneiders <a_schneide13@cs.uni-kl.de>
2019-12-03 21:11:05 +0100

    day 03 part 2

--- 03/part1and2.fs
+++ 03/part1and2.fs
@@ -32,7 +32,7 @@ let Intersetcts l1 l2 =
     let ((l2x1,l2y1),(l2x2,l2y2)) = l2
 
     let l1vert = l1x1 = l1x2
-    let l2vert = l2x1 = l2x1
+    let l2vert = l2x1 = l2x2
 
     if l1vert = l2vert then
         (0,0)
@@ -49,17 +49,54 @@ let main args =
     let directions = Array.map ParseCable input
     let cables     = Array.map DirectionsToLineSegments directions
 
-    let crosses = seq {
-        for l1 in cables.[0] do
-            for l2 in cables.[1] do
-                yield Intersetcts l1 l2
-    }
+    let crosses() =
+        seq {
+            for l1 in cables.[0] do
+                for l2 in cables.[1] do
+                    yield Intersetcts l1 l2
+        } |> Seq.filter ((<>)(0,0))
 
+    // part 1
     let closest =
-        crosses
-        |> Seq.filter ((<>)(0,0))
+        crosses()
         |> Seq.map (fun (x,y) -> abs x + abs y)
         |> Seq.min
     printfn "%i" closest
 
+    // part 2
+    let Steps c = seq {
+        for (dx,dy) in directions.[c] do
+            if dx > 0 then
+                for i = 1 to dx do
+                    yield (1,0)
+            elif dx < 0 then
+                for i = -1 downto dx do
+                    yield (-1,0)
+            elif dy > 0 then
+                for i = 1 to dy do
+                    yield (0,1)
+            elif dy < 0 then
+                for i = -1 downto dy do
+                    yield (0,-1)
+            else failwith ""
+    }
+
+    let crosses = Set.ofSeq (crosses())
+    let steps = [| Map.empty ; Map.empty |]
+
+    let CountSteps c =
+        let mutable current = (0,0)
+        let Update i d =
+            current <- (fst current + fst d, snd current + snd d)
+            if Set.contains current crosses then
+                if not (Map.containsKey current steps.[c]) then
+                    steps.[c] <- Map.add current i steps.[c]
+        Seq.iteri Update (Steps c)
+
+    CountSteps 0
+    CountSteps 1
+
+    let crossSteps = Set.map (fun c -> steps.[0].[c] + steps.[1].[c]) crosses
+    let closest = Set.minElement crossSteps
+    printfn "%i" (closest + 2)
     0
