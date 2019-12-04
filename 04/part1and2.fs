d933eb4b8b093c9f0ebd4da94da0d8e3a7849dfc
Alexander Schneiders <a_schneide13@cs.uni-kl.de>
2019-12-04 18:18:53 +0100

    day 04 part 2

--- 04/part1and2.fs
+++ 04/part1and2.fs
@@ -19,6 +19,18 @@ let HasTwoAdjacentSame (s:char array) =
         else SameAt (n+1)
     SameAt 0
 
+let HasTwoAdjacentSame' (s:char array) =
+    let ExactlyTwoAt n =
+        let isPair = s.[n] = s.[n+1]
+        let leftSame  = if (n-1) < 0 then false else s.[n-1] = s.[n]
+        let rightSame = if (n+2) >= s.Length then false else s.[n+1] = s.[n+2]
+        isPair && (not leftSame) && (not rightSame)
+    let rec SameAt n =
+        if n > (s.Length-2) then false
+        elif ExactlyTwoAt n then true
+        else SameAt (n+1)
+    SameAt 0
+
 let IsNotDecreasing (s:char array) =
     let rec IncreasesAt n =
         if n > (s.Length-2) then false
@@ -29,14 +41,21 @@ let IsNotDecreasing (s:char array) =
 let MeetsCriteria (s:char array) =
     (HasTwoAdjacentSame s) && (IsNotDecreasing s)
 
+let MeetsCriteria' (s:char array) =
+    (HasTwoAdjacentSame' s) && (IsNotDecreasing s)
+
 [<EntryPoint>]
 let main args =
-    let mutable count = 0
+    let mutable count  = 0
+    let mutable count' = 0
     do Inc input.[1]
     while input.[0] <> input.[1] do
         if (MeetsCriteria input.[0]) then
             count <- count + 1
+        if (MeetsCriteria' input.[0]) then
+            count' <- count' + 1
         do Inc input.[0]
 
     printfn "%i" count
+    printfn "%i" count'
     0
